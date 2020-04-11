280b9b5844adfce046d61fad4081ee293082cc02
Adrien <adrien@arccoza.com>
2020-04-11 20:33:49 +0900

    Remove replaced custom types

--- addons/cartographer/carto_layer.gd
+++
@@ -1,9 +0,0 @@
-tool
-extends Spatial
-class_name CartoLayer, "res://addons/cartographer/layer_icon.svg"
-
-export(Material) var material: Material
-const isCartoLayer: bool = true
-
-func _ready():
-	pass # Replace with function body.
