c865a960866e6723b7d0d9e14a1016671cfd5116
Adrien <adrien@arccoza.com>
2020-04-17 18:20:47 +0900

    Update brush raycast

--- addons/cartographer/plugin.gd
+++ addons/cartographer/plugin.gd
@@ -16,6 +16,7 @@ func _enter_tree():
 
 func _on_selection_changed(dock):
 	var selected = editor.get_selection().get_selected_nodes()
+	print(selected)
 	
 	if len(selected) == 1:
 		var node = selected[0]
@@ -76,23 +77,39 @@ func forward_spatial_gui_input(camera, event):
 		
 		var org = camera.project_ray_origin(screen_pos)
 		var dir = camera.project_ray_normal(screen_pos)
-		var pos = camera.project_position(screen_pos, 100)
 		var aabb = terrain.get_aabb()
+		var pos = raycast(org, dir)
 		
-		var i = 0
-		while i < 800:
-			i += 1
-			print(i)
-			pos = camera.project_position(screen_pos, i)
-			if pos.x >= aabb.position.x and pos.x <= aabb.end.x and pos.z >= aabb.position.z and pos.z <= aabb.end.z \
-			and pos.y < 0:
-				print("INSIDE", pos)
-				break
-		
-		var tex_pos = (aabb.size/2 + pos) * 512/20
-		tex_pos = Vector3(tex_pos.x, tex_pos.z, 0)
-		print(tex_pos)
-		terrain.material.set_shader_param("brush_tip", tex_pos)
-			
+#		var tex_pos = (aabb.size/2 + pos) * 512/20
+#		tex_pos = Vector3(int(tex_pos.x), int(tex_pos.z), 0)
+		var tex_pos = (aabb.size/2 + pos) / 20
+		tex_pos = Vector2(clamp(tex_pos.x, 0, 1), clamp(tex_pos.z, 0, 1))
+		#print(tex_pos)
+		#terrain.material.set_shader_param("brush_pos", tex_pos)
+		#terrain.get_parent().get_node("Viewport/ColorRect").material.set_shader_param("brush_pos", tex_pos)
+		terrain.get_parent().get_node("TexturePainter").paint(tex_pos, Color(1, 0, 0, 1))
 	
 	return capture_event
+
+func raycast(origin: Vector3, direction: Vector3):
+#	var space_state = terrain.get_world().direct_space_state
+#	var result = space_state.intersect_ray(origin, direction * 800)
+#	print(result.get("position"))
+	
+	var aabb = terrain.get_aabb()
+	var pos = origin
+	var intersected = false
+	
+	var i = 0
+	while i < 800:
+		i += 1
+		pos += direction
+		if pos.x >= aabb.position.x and pos.x <= aabb.end.x and pos.z >= aabb.position.z and pos.z <= aabb.end.z \
+		and pos.y < 0:
+			intersected = true
+			pos -= direction
+			break
+	
+	if intersected:
+		return pos
+	return null
