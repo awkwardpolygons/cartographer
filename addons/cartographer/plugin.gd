41274c714ccc65e4c4ea83609cc7a88773df0b76
Adrien <adrien@arccoza.com>
2020-04-22 00:04:31 +0900

    Update plugin input handling

--- addons/cartographer/plugin.gd
+++ addons/cartographer/plugin.gd
@@ -6,41 +6,33 @@ var editor = get_editor_interface()
 var terrain: CartoTerrain
 var brush: CartoBrush
 var do_paint: bool = false
-var texture_painter: TexturePainter
+
+enum {NONE, PAINT, ERASE, CLEAR}
+
 
 func _enter_tree():
-	dock = preload("res://addons/cartographer/cartographer.tscn").instance()
-	#add_control_to_dock(DOCK_SLOT_LEFT_UL, dock)
-	#add_control_to_bottom_panel(dock, "Cartographer")
-	#dock.hide()
-	#editor.get_selection().connect("selection_changed", self, "_on_selection_changed", [dock])
+	editor.get_selection().connect("selection_changed", self, "_on_selection_changed", [dock])
 
 func _on_selection_changed(dock):
 	var selected = editor.get_selection().get_selected_nodes()
-	print(selected)
-	
-	if len(selected) == 1:
-		var node = selected[0]
-		if node.get("isCartoTerrain"):
-#			make_bottom_panel_item_visible(dock)
-			node.update_layer_data()
-#		elif node.get("isCartoLayer"):
-#			make_bottom_panel_item_visible(dock)
-		else:
-			hide_bottom_panel()
-		
+#	print("selected ", selected)
+	if len(selected) == 0:
+		handles(null)
 
 func _exit_tree():
-	#remove_control_from_docks(dock)
-	#remove_control_from_bottom_panel(dock)
-	dock.free()
+	pass
 
+# TODO: Investigate MultiNodeEdit
 func handles(obj: Object):
+#	print("handles ", obj)
 	if obj is CartoBrush or obj is CartoTerrain:
 		return true
+	edit(null)
 	return false
 
 func get_terrain_from(obj: Object):
+	if obj is EditorSelection:
+		obj = obj.get_selected_nodes()[0]
 	if obj is CartoBrush:
 		obj = obj.get_parent()
 	if obj is CartoTerrain:
@@ -48,58 +40,56 @@ func get_terrain_from(obj: Object):
 	return null
 
 func get_brush_from(obj: Object):
+	if obj is EditorSelection:
+		obj = obj.get_selected_nodes()[0]
 	if obj is CartoBrush:
 		return obj
 	return null
 
 func edit(obj: Object):
+	print("EDIT", obj)
 	terrain = get_terrain_from(obj)
 	brush = get_brush_from(obj)
 
 func make_visible(visible):
-	if not visible:
-		edit(null)
+	pass
 
 func forward_spatial_gui_input(camera, event):
-	if terrain == null:
+	var action = get_action(event)
+	if action == CLEAR:
+		terrain.painter.clear()
 		return false
-	var capture_event = false
+	return try_paint(camera, action) or action
+
+func get_action(event):
+	var action = NONE
+	if event is InputEventMouseMotion:
+		action = PAINT if Input.is_mouse_button_pressed(BUTTON_LEFT) else NONE
+		action = ERASE if action and event.alt else action
+	if event is InputEventKey and event.scancode == KEY_BACKSPACE:
+		action = CLEAR
+	return action
+
+func try_paint(camera, action):
+	if action == NONE:
+		return false
+	var viewport = camera.get_viewport()
+	var viewport_container = viewport.get_parent()
+	var screen_pos = viewport.get_mouse_position() * viewport.size / viewport_container.rect_size
 	
-	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT \
-	and not event.control and not event.alt:
-		capture_event = true
-		do_paint = event.pressed
-		if texture_painter:
-			terrain.get_node("/root").remove_child(texture_painter)
-			texture_painter = null
-	elif event is InputEventMouseMotion and do_paint:
-		capture_event = true
-		var viewport = camera.get_viewport()
-		var viewport_container = viewport.get_parent()
-		var screen_pos = viewport.get_mouse_position() * viewport.size / viewport_container.rect_size
-		#var screen_pos = event.position * viewport.size / viewport_container.rect_size
-		
-		var org = camera.project_ray_origin(screen_pos)
-		var dir = camera.project_ray_normal(screen_pos)
-		var aabb = terrain.get_aabb()
-		var pos = raycast(org, dir)
-		
-		var tex_pos = (aabb.size/2 + pos) / 20
-		tex_pos = Vector2(clamp(tex_pos.x, 0, 1), clamp(tex_pos.z, 0, 1))
-		
-		if not texture_painter:
-			print("TexturePainter.new()")
-			texture_painter = TexturePainter.new()
-			terrain.material.albedo_texture = texture_painter.get_texture()
-			terrain.get_node("/root").add_child(texture_painter)
-		terrain.material.albedo_texture = texture_painter.get_texture()
-		texture_painter.paint(tex_pos, Color(1, 0, 0, 1))
-		
+	var size = Vector3(terrain.size.x, 0, terrain.size.y)
+	var org = camera.project_ray_origin(screen_pos)
+	var dir = camera.project_ray_normal(screen_pos)
+	var pos = raycast(org, dir)
 	
-	return capture_event
+	if pos:
+		var tex_pos = (size/2 + pos) / size
+		var uv = Vector2(clamp(tex_pos.x, 0, 1), clamp(tex_pos.z, 0, 1))
+		terrain.painter.paint(uv, Color(1, 0, 0, 1))
+		return true
+	return false
 
 func raycast(origin: Vector3, direction: Vector3):
 	var space_state = terrain.get_world().direct_space_state
 	var result = space_state.intersect_ray(origin, direction * 800)
-	#print(result.get("position"))
 	return result.get("position")
