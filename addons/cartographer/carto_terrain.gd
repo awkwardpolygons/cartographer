5020cf76bddcd8f7a0615116197edc919d64af8d
Adrien <adrien@arccoza.com>
2020-04-21 14:22:36 +0900

    Update terrain, add painter prop

--- addons/cartographer/carto_terrain.gd
+++ addons/cartographer/carto_terrain.gd
@@ -5,28 +5,46 @@ class_name CartoTerrain, "res://addons/cartographer/terrain_icon.svg"
 export(Vector2) var size: Vector2 = Vector2(20, 20) setget _set_size
 export(Texture) var height_map: Texture setget _set_height_map
 export(Texture) var flow_map: Texture
-const isCartoTerrain: bool = true
+
 var height_img: Image
 var csg: CSGMesh = self
 var texarr: TextureArray
+var painter: TexturePainter setget ,_get_painter
+
 
 func _set_size(s: Vector2):
 	size = s
-	csg.mesh.size = s
+	if csg.mesh:
+		csg.mesh.size = s
+
 func _set_height_map(t: Texture):
 	height_map = t
 
+func _get_painter():
+	return painter
+
 func _enter_tree():
-	#if csg == null:
-	#	csg = CSGMesh.new()
-	#	add_child(csg)
+	init_mesh()
+	init_material()
+	if Engine.is_editor_hint():
+		init_painter()
+
+func init_mesh():
 	if csg.mesh == null:
+		print("PlaneMesh.new()")
 		csg.mesh = PlaneMesh.new()
 		csg.mesh.size = size
-	
-func _ready():
-	#var this = CartoTerrain           # reference to the script
-	print(csg.mesh)
+
+func init_material():
+	if csg.material == null:
+		csg.material = SpatialMaterial.new()
+
+func init_painter():
+	if not painter:
+		painter = TexturePainter.new()
+		csg.add_child(painter)
+	if csg.material:
+		csg.material.albedo_texture = painter.get_texture()
 
 func update_layer_data():
 	var layers = get_children()
