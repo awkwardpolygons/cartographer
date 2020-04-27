b3d9b69680ba9080dc65ab6176dc4e510ed230da
Adrien <adrien@arccoza.com>
2020-04-27 23:04:10 +0900

    Remove brush resource, brushes going into new panel

--- addons/cartographer/carto_brush.gd
+++ addons/cartographer/carto_brush.gd
@@ -2,7 +2,6 @@ tool
 extends Spatial
 class_name CartoBrush, "res://addons/cartographer/layer_icon.svg"
 
-export(Resource) var brush: Resource
 export(Texture) var texture
 export(Gradient) var incline_mask
 
@@ -10,5 +9,4 @@ const isCartoBrush: bool = true
 var mask: Texture
 
 func _ready():
-	brush = PaintBrush.new()
 	pass # Replace with function body.
