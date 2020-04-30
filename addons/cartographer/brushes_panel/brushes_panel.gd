49a3c9157453ac19dad74504c75543bf7a4d2ba7
Adrien <adrien@arccoza.com>
2020-04-30 14:11:39 +0900

    Add data binding

--- addons/cartographer/brushes_panel/brushes_panel.gd
+++ addons/cartographer/brushes_panel/brushes_panel.gd
@@ -11,19 +11,23 @@ onready var BrushRotation = find_node("BrushRotation")
 onready var BrushMasks = find_node("BrushMasks")
 
 func _ready():
+	Cartographer.brushes.connect("updated", self, "_brushes_updated")
 	for ch in PaintBrush.MaskChannel:
-		
 		ChannelPicker.add_item(ch, PaintBrush.MaskChannel[ch])
 	
 	for md in PaintBrush.Mode:
 		ModePicker.add_item(md, PaintBrush.Mode[md])
+		
+	for k in Cartographer.brushes.data:
+		var br = Cartographer.brushes.get(k)
+		_add_brush_item(k, br.brush_mask)
 
 func _on_add_brush_mask_pressed():
 	AddMaskFileDialog.popup_centered_ratio(0.67)
 
 func _on_rem_brush_mask_pressed():
-	for item in BrushMasks.get_selected_items():
-		BrushMasks.remove_item(item)
+	for idx in BrushMasks.get_selected_items():
+		rem_brush_by_idx(idx)
 
 func _on_add_mask_file_dialog_files_selected(paths):
 	for path in paths:
@@ -37,7 +41,18 @@ func _on_mask_selected(index):
 	BrushRotation.disabled = false
 
 func add_brush_from_path(path: String):
-	BrushMasks.add_item(path.replace("res://", "").split("/")[-1], load(path), true)
+	var brush = Cartographer.brushes.get(path)
+	if not brush:
+		brush = PaintBrush.new(load(path))
+		Cartographer.brushes.set(path, brush)
+		_add_brush_item(path, brush.brush_mask)
 
-func rem_brush():
-	pass
+func _add_brush_item(path: String, tex: Texture):
+	BrushMasks.add_item(path.replace("res://", "").split("/")[-1], tex, true)
+	var idx = BrushMasks.get_item_count() - 1
+	BrushMasks.set_item_metadata(idx, path)
+
+func rem_brush_by_idx(idx: int):
+	var path = BrushMasks.get_item_metadata(idx)
+	BrushMasks.remove_item(idx)
+	Cartographer.brushes.rem(path)
