880c0ec93ea5ca702a4dea50dd87863d03c98fe8
Adrien <adrien@arccoza.com>
2020-04-30 14:10:13 +0900

    Add more methods, changed signal, rename brushes dict -> data, set tool mode

--- addons/cartographer/texture_painter/paint_brushes_res.gd
+++ addons/cartographer/texture_painter/paint_brushes_res.gd
@@ -1,13 +1,40 @@
+tool
 extends Resource
 class_name PaintBrushes
 
-export(Dictionary) var brushes: Dictionary = {}
+export(Dictionary) var data: Dictionary = {}
+
+enum Op {CLEAR, ADD, SET, REM}
+
+signal updated
+
+func clear():
+	emit_signal("updated", Op.CLEAR, null, null)
+	data.clear()
+
+func is_empty() -> bool:
+	return data.empty()
 
 func set(k: String, v: PaintBrush):
-	brushes[k] = v
+	var has = data.has(k)
+	data[k] = v
+	emit_signal("updated", Op.SET if has else Op.ADD, k, v)
 
-func get(k: String) -> PaintBrush:
-	return brushes[k]
+func get(k: String, default=null) -> PaintBrush:
+	return data.get(k, default)
 
 func rem(k: String) -> bool:
-	return brushes.erase(k)
+	var val = data.get(k)
+	var ret = data.erase(k)
+	if ret:
+		emit_signal("updated", Op.REM, k, val)
+	return ret
+
+func has(k: String) -> bool:
+	return data.has(k)
+
+func has_all(keys: Array) -> bool:
+	return data.has_all(keys)
+
+func hash() -> int:
+	return data.hash()
