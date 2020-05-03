cb061f8a6e3da1e92156fb0e0d9e946710043de7
Adrien <adrien@arccoza.com>
2020-05-04 01:19:26 +0900

    Add active_brush prop and signal

--- addons/cartographer/cartographer_singleton.gd
+++ addons/cartographer/cartographer_singleton.gd
@@ -2,9 +2,19 @@ tool
 extends Node
 
 var brushes: PaintBrushes
+var active_brush: PaintBrush setget _set_active_brush, _get_active_brush
+
+signal active_brush_changed
 
 func _init():
 	self.load()
+
+func _set_active_brush(br: PaintBrush):
+	active_brush = br
+	emit_signal("active_brush_changed", br)
+
+func _get_active_brush():
+	return active_brush
 
 func load():
 	if ResourceLoader.exists("res://addons/cartographer/data/brushes.tres"):
