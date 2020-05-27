ac4f0e8bd79541bbf23fafc9232014f17c847153
Adrien <adrien@arccoza.com>
2020-05-27 12:02:53 +0900

    Add separate painters for height and masks

--- addons/cartographer/terrain/carto_terrain.gd
+++ addons/cartographer/terrain/carto_terrain.gd
@@ -7,7 +7,8 @@ export(Resource) var terrain_layers
 
 var terrain: MeshInstance = self
 var bounds: CartoTerrainBounds
-var painter: TexturePainter setget , _get_painter
+var mask_painter: TexturePainter setget , _get_mask_painter
+var height_painter: TexturePainter setget , _get_height_painter
 var data_dir: Directory setget , _get_data_dir
 var _shader = preload("res://addons/cartographer/terrain/terrain.shader")
 
@@ -15,16 +16,22 @@ func _set_size(s: Vector3):
 	size = s
 	bounds.reset(transform.origin - Vector3(size.x/2, 0, size.z/2), size)
 	set_custom_aabb(bounds._aabb)
-	if terrain.mesh:
-		terrain.mesh.size = Vector2(s.x, s.z)
-	if terrain.material_override:
-		terrain.material_override.set_shader_param("terrain_size", s)
+#	if terrain.mesh:
+#		terrain.mesh.size = Vector2(s.x, s.z)
+#	if terrain.material_override:
+#		terrain.material_override.set_shader_param("terrain_size", s)
 
-func _get_painter():
-	if not painter:
-		if has_node("TexturePainter"):
-			painter = get_node("TexturePainter")
-	return painter
+func _get_mask_painter():
+	if not mask_painter:
+		if has_node("MaskPainter"):
+			mask_painter = get_node("MaskPainter")
+	return mask_painter
+
+func _get_height_painter():
+	if not height_painter:
+		if has_node("HeightPainter"):
+			height_painter = get_node("HeightPainter")
+	return height_painter
 
 func _get_data_dir():
 	if data_dir != null:
@@ -58,7 +65,7 @@ func _enter_tree():
 	_init_mesh()
 	_init_material()
 	if Engine.is_editor_hint():
-		_init_painter()
+		_init_painters()
 
 func _init_mesh():
 	if terrain.mesh == null:
@@ -73,26 +80,45 @@ func _init_material():
 		terrain.material_override.shader = _shader
 		terrain.material_override.set_shader_param("terrain_textures", terrain_layers.textures)
 
-func _init_painter():
-	if not painter:
-		print("TexturePainter.new()")
-		painter = TexturePainter.new()
-		painter.name = "TexturePainter"
-		terrain.add_child(painter)
+func _init_painters():
+	_init_mask_painter()
+	_init_height_painter()
 	if terrain.material_override:
 		terrain.material_override.set_shader_param("terrain_size", size)
-		terrain.material_override.set_shader_param("terrain_masks", painter.get_texture())
+		terrain.material_override.set_shader_param("terrain_masks", mask_painter.get_texture())
+		terrain.material_override.set_shader_param("terrain_height", height_painter.get_texture())
 
-func paint(action: int, pos: Vector2):
-	if not painter:
+func _init_mask_painter():
+	if not mask_painter:
+		print("TexturePainter.new()")
+		mask_painter = TexturePainter.new()
+		mask_painter.name = "MaskPainter"
+		terrain.add_child(mask_painter)
+
+func _init_height_painter():
+	if not height_painter:
+		print("TexturePainter.new()")
+		height_painter = TexturePainter.new()
+#		height_painter.hdr = true
+		height_painter.name = "HeightPainter"
+		terrain.add_child(height_painter)
+
+func paint_masks(action: int, pos: Vector2):
+	if not mask_painter:
 		return
-	painter.paint_masks(action, pos, terrain_layers.selected)
+	mask_painter.paint_masks(action, pos, terrain_layers.selected)
+
+func paint_height(action: int, pos: Vector2):
+	if not height_painter:
+		return
+	height_painter.paint_height(action, pos)
 
 func intersect_ray(from: Vector3, dir: Vector3):
-	var hmap = ImageTexture.new()
-	var img = Image.new()
-	img.create(1024, 1024, Image.FORMAT_RGBA8, 0)
-	img.fill(Color(0, 0, 0, 1))
-	hmap.create_from_image(img)
+#	var hmap = ImageTexture.new()
+#	var img = Image.new()
+#	img.create(1024, 1024, Image.FORMAT_RGBA8, 0)
+#	img.fill(Color(0, 0, 0, 1))
+#	hmap.create_from_image(img)
+	var hmap = height_painter.get_texture()
 	from = transform.xform_inv(from)
 	return bounds.intersect_ray(from, dir, hmap)
