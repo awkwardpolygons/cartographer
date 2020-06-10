bce45f6a684a486bc348e55fde9fa56b699047d4
Adrien <adrien@arccoza.com>
2020-06-10 13:32:22 +0900

    Tidy up

--- addons/cartographer/terrain/carto_terrain_inspector.gd
+++ addons/cartographer/terrain/carto_terrain_inspector.gd
@@ -3,18 +3,6 @@ class_name CartoTerrainInspector
 
 const LayersEditor = preload("res://addons/cartographer/terrain/carto_terrain_layers_editor/layers_editor.gd")
 
-class Test extends EditorProperty:
-	func _ready():
-		prints("Test:", get_edited_object(), get_edited_property())
-		label = "Textures"
-		var box = VBoxContainer.new()
-		var btn = Button.new()
-		btn.text = "Button"
-		box.add_child(btn)
-		box.rect_min_size = Vector2(0, 300)
-		add_child(box)
-		set_bottom_editor(box)
-
 func can_handle(object):
 	return object is CartoTerrainLayers
 
@@ -30,10 +18,3 @@ func parse_property(object, type, path, hint, hint_text, usage):
 		return true
 	
 	# TODO: Cache the editor
-#	var prop = object.get(path)
-#	if prop is CartoTerrainLayers:
-#		var ed = CartoTerrainLayersEditor.new()
-#		ed.visible = true
-#		add_property_editor("terrain_layers", ed)
-#		return true
-#	return false
