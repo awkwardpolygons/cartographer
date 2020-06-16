25aff6715239cdb4a2a8601efa7b08a84ca2a706
Adrien <adrien@arccoza.com>
2020-06-16 16:30:13 +0900

    Update inspector to use CartoTerrainMaterial

--- addons/cartographer/terrain/carto_terrain_inspector.gd
+++ addons/cartographer/terrain/carto_terrain_inspector.gd
@@ -1,10 +1,10 @@
 extends EditorInspectorPlugin
 class_name CartoTerrainInspector
 
-const LayersEditor = preload("res://addons/cartographer/terrain/carto_terrain_layers_editor/layers_editor.gd")
+const LayersEditor = preload("res://addons/cartographer/terrain/carto_terrain_material_editor/layers_editor.gd")
 
 func can_handle(object):
-	return object is CartoTerrainLayers
+	return object is CartoTerrainMaterial
 
 func parse_property(object, type, path, hint, hint_text, usage):
 #	prints(path, object, object.get(path), type, hint, hint_text, usage)
@@ -12,7 +12,7 @@ func parse_property(object, type, path, hint, hint_text, usage):
 		return false
 	
 	if path == "textures":
-		add_property_editor_for_multiple_properties("Textures", PoolStringArray(["textures", "use_triplanar"]), LayersEditor.new())
+		add_property_editor_for_multiple_properties("Material", PoolStringArray(["textures", "use_triplanar"]), LayersEditor.new())
 		return true
 	elif path == "selected" or path == "use_triplanar":
 		return true
