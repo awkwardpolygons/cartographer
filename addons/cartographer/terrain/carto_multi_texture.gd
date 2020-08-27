af4ee1cdb949f8551ab357d1587555800936419b
Adrien <adrien@arccoza.com>
2020-08-27 11:36:46 +0800

    Update CartoMultiTexture, add set_layer, get_size methods

--- addons/cartographer/terrain/carto_multi_texture.gd
+++ addons/cartographer/terrain/carto_multi_texture.gd
@@ -1,3 +1,17 @@
 tool
 extends TextureArray
 class_name CartoMultiTexture
+
+func get_size():
+	return Vector2(get_width(), get_height())
+
+func set_layer(src, idx: int):
+	if src is Texture:
+		src = src.get_data()
+	if src.get_size() != get_size():
+		src.resize(get_width(), get_height())
+	src.decompress()
+	if src.get_format() != get_format():
+#		src.decompress()
+		src.convert(get_format())
+	set_layer_data(src, idx)
