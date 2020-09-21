a400b20aca77250a5edcec91f68eecb2228a1a1c
Adrien <adrien@arccoza.com>
2020-09-22 00:55:51 +0800

    Add FlagsEditor, a compact custom flags editor

--- addons/cartographer/terrain/carto_terrain_inspector.gd
+++ addons/cartographer/terrain/carto_terrain_inspector.gd
@@ -3,11 +3,12 @@ class_name CartoTerrainInspector
 
 # const LayersEditor = preload("res://addons/cartographer/terrain/carto_terrain_material_editor/layers_editor.gd")
 const MultiTextureEditor = preload("res://addons/cartographer/terrain/carto_multi_texture_editor/editor.gd")
-const skip_props = ["selected", "use_triplanar", "shader"]
+const FlagsEditor = preload("res://addons/cartographer/ui/flags_editor.gd")
+const skip_props = ["selected", "shader"]
 var undo_redo: UndoRedo
 
 func can_handle(object):
-	return object is CartoMultiTexture
+	return object is CartoMultiTexture or object is CartoTerrainMaterial
 
 func parse_property(object, type, path, hint, hint_text, usage):
 #	prints(path, object, object.get(path), type, hint, hint_text, usage)
@@ -21,7 +22,10 @@ func parse_property(object, type, path, hint, hint_text, usage):
 #		add_property_editor_for_multiple_properties("Layers", PoolStringArray(["data"]), mted)
 #		add_property_editor("data", mted)
 		add_custom_control(mted)
+	elif object is CartoTerrainMaterial and hint == PROPERTY_HINT_FLAGS:
+		var flags_ed = FlagsEditor.new()
+		flags_ed.hint_text = hint_text
+		add_property_editor(path, flags_ed)
+		return true
 	elif path in skip_props:
 		return true
-	
-	# TODO: Cache the editor
