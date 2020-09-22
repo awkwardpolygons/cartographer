2cdbb8717c2e26e73267068a2005fedea81e4823
Adrien <adrien@arccoza.com>
2020-09-22 11:42:00 +0800

    Improve layout, and re-rendering in update_property

--- addons/cartographer/ui/flags_editor.gd
+++ addons/cartographer/ui/flags_editor.gd
@@ -2,30 +2,43 @@ tool
 extends EditorProperty
 
 var hint_text: String
+var box: CartoAxisLayout
 var grid: GridContainer
 
 func _init():
+	size_flags_horizontal = SIZE_FILL
+	box = CartoAxisLayout.new()
 	grid = GridContainer.new()
+	grid.size_flags_horizontal = SIZE_FILL
 	grid.columns = 8
-	add_child(grid)
-	set_bottom_editor(grid)
+
+func _ready():
+	box.add_child(grid)
+	add_child(box)
+	set_bottom_editor(box)
 
 func update_property():
 	var val: int = get_edited_object().get(get_edited_property())
 	var items = hint_text.split(",")
-	var count = len(items)
+	var have = grid.get_child_count()
+	var want = len(items)
 	
-	for ch in grid.get_children():
-		grid.remove_child(ch)
-		ch.queue_free()
-	
-	for i in count:
+	for i in want:
+		if i >= have:
+			grid.add_child(CheckBox.new())
 		var flg: int = pow(2, i)
-		var chk = CheckBox.new()
-		grid.add_child(chk)
+		var chk = grid.get_child(i)
+		chk.toggle_mode = true
 		chk.pressed = val & flg
 		chk.set_tooltip(items[i])
+		if chk.is_connected("toggled", self, "_on_toggle"):
+			chk.disconnect("toggled", self, "_on_toggle")
 		chk.connect("toggled", self, "_on_toggle", [i])
+	
+	for i in have - want:
+		var chk = grid.get_child(have - i - 1)
+		grid.remove_child(chk)
+		chk.queue_free()
 
 func _on_toggle(on, idx):
 	var val: int = get_edited_object().get(get_edited_property())
