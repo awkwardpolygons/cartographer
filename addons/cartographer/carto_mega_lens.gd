c88d20e97e0f77fbb152c0368c1be874c38dc6b9
Adrien <adrien@arccoza.com>
2021-03-30 14:25:47 +0800

    Update mega lens

--- addons/cartographer/carto_mega_lens.gd
+++ addons/cartographer/carto_mega_lens.gd
@@ -3,9 +3,9 @@ extends Node2D
 class_name CartoMegaLens
 
 export(int) var tiers: int = 3 setget set_tiers
-export(int) var lens_resolution: int = 1500 setget set_lens_resolution
-export(int) var lens_scale_base: int = 2 setget set_lens_scale_base
-export(int) var tile_resolution: int = 2048
+export(int) var tiers_radix: int = 2 setget set_tiers_radix
+export(int) var lens_resolution: int = 2048 setget set_lens_resolution
+export(float) var render_scale: float = 1.0 setget set_render_scale
 export(Vector2) var offset: Vector2 = Vector2(0, 0) setget scroll
 
 var world_2d: World2D
@@ -14,19 +14,24 @@ var root: Viewport
 func set_tiers(v: int):
 	tiers = v if v > 0 else 1
 	update_lenses()
+	scroll(offset)
 
 func set_lens_resolution(v: int):
 	lens_resolution = v
 	update_lenses()
+	scroll(offset)
 
-func set_lens_scale_base(v: int):
-	lens_scale_base = v
+func set_tiers_radix(v: int):
+	tiers_radix = v
+	scroll(offset)
+
+func set_render_scale(v: float):
+	render_scale = v
 	scroll(offset)
 
 func _init():
 	world_2d = World2D.new()
 	scroll(offset)
-	set_process(false)
 
 func _enter_tree():
 #	world_2d = get_tree().root.world_2d
@@ -37,27 +42,31 @@ func _enter_tree():
 func _ready():
 	scroll(offset)
 
-func _physics_process(delta):
-	var cam = get_tree().root.get_camera()
-	cam = cam if cam else Cartographer.editor_camera
-	var loc = cam.get_camera_transform().origin
-#	prints("-->", loc)
-	var stride = 32.0
-	var off = Vector2(loc.x, loc.z)
-	off = (off / stride).floor() * stride
-#	prints(off)
-	scroll(off)
+#func _physics_process(delta):
+#	var cam = get_tree().root.get_camera()
+#	cam = cam if cam else Cartographer.editor_camera
+#	var loc = cam.get_camera_transform().origin
+##	prints("-->", loc)
+#	var stride = 32.0
+#	var off = Vector2(loc.x, loc.z)
+#	off = (off / stride).floor() * stride
+##	prints(off)
+#	scroll(off)
 
 func update_lenses():
+	if not is_inside_tree():
+		return
+	
 	var have = get_child_count()
 	var want = tiers
+	var children = get_children()
 	
 	for i in have:
-		apply_lens_props(get_lens(i), i)
+		apply_lens_props(children[i], i)
 	for i in range(have, want):
 		add_lens()
 	for i in range(want, have):
-		remove_lens(i)
+		remove_lens(children[i])
 	
 	root = get_lens(0)
 
@@ -67,8 +76,9 @@ func add_lens():
 	add_child(lens)
 	apply_lens_props(lens, idx)
 
-func remove_lens(idx: int):
-	remove_child(get_child(idx))
+func remove_lens(lens: Node):
+	remove_child(lens)
+	lens.queue_free()
 
 func get_lens(idx: int):
 	return get_child(idx)
@@ -88,10 +98,10 @@ func apply_lens_props(lens: Viewport, idx: int):
 
 func scroll(_offset: Vector2):
 	offset = _offset
-	if is_inside_tree() == null:
+	if not is_inside_tree():
 		return
 	
 	for i in get_child_count():
 		var lens = get_child(i)
-		var scale = 1.0/pow(lens_scale_base, i)
+		var scale = 1.0/pow(tiers_radix, i) * render_scale
 		lens.canvas_transform = Transform2D(Vector2(scale, 0), Vector2(0, scale), -offset * scale)
